45fb7ee0106eba46fe3e53a06bc757875ea1ad35
zhangliming <lemom211@outlook.com>
2021-03-10 18:12:42 +0800

    feat: update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,14 @@
 import style from "./style.css";
 import { Link } from "preact-router/match";
+import douyin from '../../assets/images/douyin.png'
+import weixin from '../../assets/images/weixin.png'
+import weibo from '../../assets/images/weibo.png'
+import github from '../../assets/images/github.png'
 
 const Footer = () => {
   return (
     <div class={style.footer}>
-      <div style={{ display: "flex", flexDirection: "column",flex:1 }}>
+      <div style={{ display: "flex", flexDirection: "column" }}>
         <div
           style={{
             fontSize: "40px",
@@ -21,26 +25,40 @@ const Footer = () => {
           <div>京ICP备17045054号-10</div>
         </div>
       </div>
-      <div style={{flex:2}}>
+      <div style={{ marginBottom: "40px"}}>
         <Link class={style.link} href="/">
           关于我们
         </Link>
-        <span style={{ margin: "0 10px" }}>/</span>
+        <span style={{ margin: "0 20px" }}>/</span>
         <Link class={style.link} href="/">
           开放资源
         </Link>
-        <span style={{ margin: "0 10px" }}>/</span>
+        <span style={{ margin: "0 20px" }}>/</span>
         <Link class={style.link} href="/">
           用户协议
         </Link>
-        <span style={{ margin: "0 10px" }}>/</span>
+        <span style={{ margin: "0 20px" }}>/</span>
         <Link class={style.link} href="/">
           隐私协议
         </Link>
-        <span style={{ margin: "0 10px" }}>/</span>
+        <span style={{ margin: "0 20px" }}>/</span>
         <Link class={style.link} href="/">
           侵权投诉
         </Link>
+      </div>
+      <div style={{ marginBottom: "40px", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+        <div>
+            <img src={douyin} />
+        </div>
+        <div style={{margin:"0 20px"}}>
+            <img src={weixin}/>
+        </div>
+        <div>
+            <img src={weibo}/>
+        </div>
+        <div style={{margin:"0 100px 0 20px"}}>
+            <img src={github} style={{height:"48px"}}/>
+        </div>
       </div>
     </div>
   );
