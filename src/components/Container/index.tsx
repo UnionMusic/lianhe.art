b4d24c02ad1e932ac8f89180fb59d4774d8838be
zhangliming <lemom211@outlook.com>
2021-03-15 15:53:54 +0800

    feat: add responsive

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,9 +1,12 @@
 import { h } from "preact";
 import style from "./style.module.scss";
+import {useContext} from "preact/hooks";
+import {ResponsiveContext} from "../app";
 
 const Container = (props: { children: any }) => {
+  const responsive = useContext(ResponsiveContext)
   const { children } = props;
-  return <div class={style.container}>{children}</div>;
+  return <div class={responsive?style.container_1440:style.container}>{children}</div>;
 };
 
 export default Container;
