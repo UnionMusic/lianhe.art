432779737b5cd6b271c3e5fe8021f4adb6e9af10
Teakowa Gatanothor O'deorain <hub+git@teakowa.dev>
2021-05-18 21:31:40 +0800

    build(next): add prod static file url

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
+  env: {
+    STATIC_URL: isProd ? process.env.STATIC_URL : ''
+  },
+  assetPrefix: isProd ? process.env.STATIC_URL : '',
   poweredByHeader: false,
   future: {
     webpack5: true,
