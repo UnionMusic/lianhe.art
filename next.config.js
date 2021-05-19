9c12acd1b0cc39ba5ed7d22e06e8b9c8c476a973
zhangliming <lemom211@outlook.com>
2021-05-19 17:46:32 +0800

    fix: image cdn

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 const isProd = process.env.NODE_ENV === 'production'
-const STATIC_URL = "https://static.lianhe.art";
+const STATIC_URL = 'https://static.lianhe.art'
 
 module.exports = {
   env: {
-    STATIC_URL: isProd ? STATIC_URL : ''
+    STATIC_URL: isProd ? STATIC_URL : '',
   },
   assetPrefix: isProd ? STATIC_URL : '',
   poweredByHeader: false,
   future: {
     webpack5: true,
+  },
+  images: {
+    domains: [STATIC_URL],
   },
   distDir: 'dist',
 }
