6cbe54b27f939c5d0793fa40393f9518c9b42225
Teakowa Gatanothor O'deorain <hub+git@teakowa.dev>
2021-05-19 22:21:34 +0800

    build(next): add serverless support

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const STATIC_URL = 'https://static.lianhe.art'
 
 module.exports = {
+  target: 'serverless',
   env: {
     STATIC_URL: isProd ? STATIC_URL : '',
   },
