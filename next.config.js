b00948ec9f9a286879f9ebc58121930606bb0016
Teakowa Gatanothor O'deorain <hub+git@teakowa.dev>
2021-05-20 17:06:48 +0800

    chore: add @next/bundle-analyzer support [skip ci]

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 const isProd = process.env.NODE_ENV === 'production'
 const STATIC_URL = 'https://static.lianhe.art'
 
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
   target: 'serverless',
   env: {
     STATIC_URL: isProd ? STATIC_URL : '',
@@ -15,4 +19,4 @@ module.exports = {
     domains: [STATIC_URL],
   },
   distDir: 'dist',
-}
+})
